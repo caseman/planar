854789a0fadeb39abbe9ab5d983897ab576f32ef
Casey Duncan <casey.duncan@gmail.com>
2010-04-06 17:39:39 -0600

    tabs -> spaces, add vim config lines

--- lib/planar/util.py
+++ lib/planar/util.py
@@ -1,12 +1,15 @@
 
 def cached_property(func):
 
-	def getter(self, name=func.func_name):
-		try:
-			return self.__dict__[name]
-		except KeyError:
-			self.__dict__[name] = value = func(self)
-			return value
-	
-	getter.func_name = func.func_name
-	return property(getter, doc=func.func_doc)
+    def getter(self, name=func.func_name):
+        try:
+            return self.__dict__[name]
+        except KeyError:
+            self.__dict__[name] = value = func(self)
+            return value
+    
+    getter.func_name = func.func_name
+    return property(getter, doc=func.func_doc)
+
+# vim: ai ts=4 sts=4 et sw=4 tw=78
+
