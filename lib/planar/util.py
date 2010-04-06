7f2df5e517f0b93464171207fb05c207f1f70ba1
Casey Duncan <casey.duncan@gmail.com>
2010-04-06 01:29:44 -0600

    add util module and test

---
+++ lib/planar/util.py
@@ -0,0 +1,12 @@
+
+def cached_property(func):
+
+	def getter(self, name=func.func_name):
+		try:
+			return self.__dict__[name]
+		except KeyError:
+			self.__dict__[name] = value = func(self)
+			return value
+	
+	getter.func_name = func.func_name
+	return property(getter, doc=func.func_doc)
