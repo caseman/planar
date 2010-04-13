255e27985552f923cfc3186f112740a744a0682e
Casey Duncan <casey.duncan@gmail.com>
2010-04-12 23:20:31 -0600

    Python vector implementation

---
+++ lib/planar/__init__.py
@@ -0,0 +1,19 @@
+__all__ = ('set_epsilon', 'Vec2')
+__version__ = (0, 1, 0)
+
+from planar.vector import Vec2
+
+def set_epsilon(epsilon):
+    """Set the global limit value for floating point comparisons. 
+
+    The default value of ``0.00001`` is suitable for values
+    that are in the "counting range". You may need a larger
+    epsilon when using large absolute values, and a smaller value
+    for very small values close to zero. Otherwise approximate
+    comparison operations will not work as expected.
+    """
+    global EPSILON, EPSILON2
+    EPSILON = float(epsilon)
+    EPSILON2 = EPSILON**2
+
+set_epsilon(0.00001)
--- lib/planar/util.py
+++ lib/planar/util.py
@@ -1,3 +1,16 @@
+#############################################################################
+#
+# Copyright (c) 2010 by Casey Duncan and contributors
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the MIT License
+# A copy of the license should accompany this distribution.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+#############################################################################
+
 
 def cached_property(func):
 
@@ -10,6 +23,7 @@ def cached_property(func):
     
     getter.func_name = func.func_name
     return property(getter, doc=func.func_doc)
+
 
 # vim: ai ts=4 sts=4 et sw=4 tw=78
 
